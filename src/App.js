0ead44eafa4725760bac8feb8447c2759674ff5b
fahim-19 <syedfahim300701@gmail.com>
2023-03-22 11:56:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // let count=0;
+  const [count,setCount]=useState(0);
+  function decreaseHandler(){
+    // count=count-1;   re-render on UI nhi hoga
+    setCount(count-1);  //ab hoga
+  }
+
+  function increaseHandler(){
+    // count=count+1;   re-render on UI nhi hoga
+    setCount(count+1);  //ab hoga
+  }
+
+  function resetHandler(){
+    setCount(0);
+  }
+
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">Increment & Decrement</div>
+      <div className="flex bg-white flex-row justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button onClick={decreaseHandler} className=" border-r-2 text-center w-20 border-[#bfbfbf] text-5xl" >
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl" >
+          {count}
+        </div>
+        <button onClick={increaseHandler} className=" border-l-2 text-center w-20 border-[#bfbfbf] text-5xl" >
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className="text-white bg-[#0398d4] px-5 py-2 rounded-md text-lg">
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
